1c92dc0fd6f29673eaf187c92fb0432434b94d05
ozex-co <ozex.me.11@gmail.com>
2025-03-19 23:01:29 +0200

    new  update

--- config/configanalyticsDatabase.js
+++
@@ -1,9 +0,0 @@
-const { Sequelize } = require("sequelize");
-
-const analyticsDB = new Sequelize({
-    dialect: "sqlite",
-    storage: "./analytics.sqlite", // قاعدة بيانات منفصلة
-    logging: false, // تعطيل السجلات لتحسين الأداء
-});
-
-module.exports = analyticsDB;
--- routes/analyticsRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const { trackVisitor, getAnalytics } = require("../controllers/analyticsController");
-
-router.post("/track", trackVisitor);
-router.get("/stats", getAnalytics);
-
-module.exports = router;
