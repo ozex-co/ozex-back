0047b601f10de6da51a0c94daa36af897d9dfdb2
ozex-co <ozex.me.11@gmail.com>
2025-03-27 17:08:42 +0200

    Update Page.js

--- models/Page.js
+++ models/Page.js
@@ -3,31 +3,64 @@ const sequelize = require("../config/pagesDatabase");
 
 const Page = sequelize.define("Page", {
   id: { 
-    type: DataTypes.INTEGER, 
-    primaryKey: true, 
-    autoIncrement: true 
+    type: DataTypes.INTEGER,
+    primaryKey: true,
+    autoIncrement: true
   },
-  title: { 
-    type: DataTypes.STRING, 
+  title: {
+    type: DataTypes.STRING,
     allowNull: false,
     validate: {
-      notEmpty: true
+      notEmpty: {
+        msg: "عنوان الصفحة مطلوب"
+      },
+      len: {
+        args: [3, 100],
+        msg: "يجب أن يكون العنوان بين 3 و100 حرف"
+      }
     }
   },
-  vueComponentCode: { 
-    type: DataTypes.TEXT, 
-    allowNull: false 
+  vue_component_code: {
+    type: DataTypes.TEXT,
+    allowNull: false,
+    validate: {
+      notEmpty: {
+        msg: "كود المكون مطلوب"
+      }
+    }
   },
-  metaTags: { 
+  meta_tags: {
     type: DataTypes.JSON,
-    defaultValue: {} 
+    defaultValue: {},
+    validate: {
+      isValidMetaTags(value) {
+        try {
+          JSON.parse(JSON.stringify(value));
+        } catch (e) {
+          throw new Error("بيانات Meta Tags غير صالحة");
+        }
+      }
+    }
   },
-  imageUrl: DataTypes.STRING
+  image_url: {
+    type: DataTypes.STRING,
+    validate: {
+      isUrl: {
+        msg: "يجب أن يكون رابط الصورة صالحاً"
+      }
+    }
+  }
 }, {
-  tableName: 'Pages', // تحديد اسم الجدول بشكل صريح
+  tableName: 'pages',
   timestamps: true,
   createdAt: 'created_at',
-  updatedAt: 'updated_at'
+  updatedAt: 'updated_at',
+  paranoid: true // للحذف اللين
 });
+
+// علاقات النموذج (إذا وجدت)
+// Page.associate = models => {
+//   Page.belongsTo(models.User, { foreignKey: 'user_id' });
+// };
 
 module.exports = Page;
